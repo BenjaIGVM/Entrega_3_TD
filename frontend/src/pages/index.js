4e449b2c23d744be451942fc1b6023915bcd4de2
NikoGB <114112855+NikoGB@users.noreply.github.com>
2023-12-11 22:31:20 -0300

    Add files via upload

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 function DogTinder() {
+    const router = useRouter();
 
 
     return (
 
-        <div className="grid lg:grid-cols-3 grid-cols-1 text-black gap-4">
-            <h1>INDEX</h1>
+        <div className="flex items-center justify-center my-auto h-screen mx-auto w-screen text-black gap-4">
+            <button onClick={()=> router.push(`/Register`)} className="bg-green-500 text-white text-[40px] hover:bg-green-950 px-10 py-6 rounded-full mt-4">
+                Registrar
+            </button>
+            
+            <button onClick={()=> router.push(`/DogsList`)} className="bg-green-500 text-white text-[40px] hover:bg-green-950 px-10 py-6 rounded-full mt-4">
+                Seleccionar Perro
+            </button>
 
         </div >
     );
