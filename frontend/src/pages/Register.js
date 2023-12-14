759c985b864c6caf6c0266e232c0a1d0662ce65c
BenjaIGVM <115327548+BenjaIGVM@users.noreply.github.com>
2023-12-14 13:25:31 -0300

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { PerroCall } from '../utils/calls';
+import { PerroCall } from '@/utils/calls';
 import RandomCard from '@/components/RandomCard';
+
+
 const Register = () => {
     const [dogData, setDogData] = useState(null);
     const [dogName, setDogName] = useState('');
@@ -31,12 +33,16 @@ const Register = () => {
                 setErrors({});
             } else {
                 console.error('Error al obtener perro aleatorio o imagen no válida');
+    
+                // Intenta obtener otra imagen aleatoria después de 1 segundo (1000 milisegundos)
                 setTimeout(() => {
                     fetchDog();
                 }, 1000);
             }
         } catch (error) {
             console.error('Error en la llamada a la API de perros:', error);
+    
+            // Intenta obtener otra imagen aleatoria después de 1 segundo (1000 milisegundos)
             setTimeout(() => {
                 fetchDog();
             }, 1000);
@@ -50,65 +56,74 @@ const Register = () => {
         try {
             setErrors({});
             if (!dogName || !/^[A-Za-z\s]+$/.test(dogName)) {
-                console.error('El nombre es obligatorio y debe ser un string alfabético.');
+                setErrors({ name: 'El nombre es obligatorio y debe ser un string alfabético.' });
                 return;
             }
-            
+
             if (!dogDescription || !/^[A-Za-z\s.,]+$/.test(dogDescription)) {
-                console.error('La descripción es obligatoria y no debe contener números ni caracteres especiales.');
+                setErrors({
+                    description: 'La descripción es obligatoria y no debe contener números ni caracteres especiales.',
+                });
                 return;
             }
-            console.log('imagen', dogData?.message);
+
             const imageUrl = dogData?.message;
             if (!imageUrl) {
-                console.error('No se pudo obtener la URL de la imagen del perro.');
+                setErrors({ imageUrl: 'No se pudo obtener la URL de la imagen del perro.' });
                 return;
             }
-            const registerResponse = await perroCall('create', 'POST', {
+
+            const registerResponse = await PerroCall('create', 'POST', {
                 name: dogName,
                 url: imageUrl,
                 description: dogDescription,
             });
+
             if (registerResponse.error) {
-                console.error('Error al registrar perro:', registerResponse.error);
-                setErrors(registerResponse.error);
+                setErrors({ registerError: `Error al registrar perro: ${registerResponse.error}` });
             } else {
-                console.log('Perro creado correctamente.');
                 setSuccessMessage('Perro creado correctamente.');
                 setDogName('');
                 setDogDescription('');
                 fetchDog();
+
+                // Desaparecer el mensaje de éxito después de 3 segundos (3000 milisegundos)
+                setTimeout(() => {
+                    setSuccessMessage('');
+                }, 3000);
             }
         } catch (error) {
             console.error('Error al registrar perro:', error);
+            setErrors({ registerError: `Error al registrar perro: ${error.message}` });
         }
     };
+
     return (
-        <div className="text-black gap-4 h-screen flex items-center justify-center ">
-            <div className="col-span-1 p-4 items-center h-min">
+        <div className="text-black gap-4 h-screen flex items-center justify-center">
+            <div className="col-span-1 p-4 items-center max-w-md w-full">
                 <h2 className="text-xl font-semibold mb-4 self-center text-center">Registre su perro</h2>
                 {dogData && (
                     <div className="flex flex-col items-center">
-                       <RandomCard imageUrl={dogData.message}  />
+                        <RandomCard imageUrl={dogData.message} className="mb-4 w-full max-w-md" />
                         <button onClick={handleFetchAnotherDog} className="bg-blue-500 text-white px-4 py-2 rounded-full mb-2">
                             Cambiar Imagen
                         </button>
-                        <div>
+                        <div className="mb-2 w-full max-w-md">
                             <label className="block mb-2 text-black">Nombre:</label>
                             <input
                                 type="text"
                                 value={dogName}
                                 onChange={(e) => setDogName(e.target.value)}
-                                className="border rounded px-3 py-2 w-full mb-2"
+                                className="border rounded px-3 py-2 w-full"
                             />
                             {errors && errors.name && <p className="text-red-500">{errors.name}</p>}
                         </div>
-                        <div>
+                        <div className="mb-2 w-full max-w-md">
                             <label className="block mb-2 text-black">Descripción:</label>
                             <textarea
                                 value={dogDescription}
                                 onChange={(e) => setDogDescription(e.target.value)}
-                                className="border rounded px-3 py-2 w-full mb-2"
+                                className="border rounded px-3 py-2 w-full"
                             />
                             {errors && errors.description && <p className="text-red-500">{errors.description}</p>}
                         </div>
